f334bf917c7ef6f7e63ff52c517e47e71f80350c
Karrius <Ecetris@gmail.com>
2023-02-16 22:30:00 +0100

    regitserForm rewritten using tanstack react query

--- components/registerForm/registerForm.tsx
+++ components/registerForm/registerForm.tsx
@@ -1,22 +1,31 @@
+import { useMutation } from "@tanstack/react-query";
 import { Form, Formik } from "formik";
-import { useState } from "react";
 
 import { TextInput } from "../textInput/textInput";
 
+interface formValues {
+    readonly email: string,
+    readonly password: string,
+}
 
 export const RegisterForm = () => {
-    const [errorMessage, setErrorMessage] = useState('')
+
+    const createUserMutation = useMutation({
+        mutationFn: async (values: formValues) => {
+            const res = await fetch('http://localhost:3000/api/user/create', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(values)
+            })
+            if (res.status !== 200) throw new Error(res.statusText)
+        },
+    })
 
     return (
         <Formik
             initialValues={{ email: '', password: '' }}
-            onSubmit={async values => {
-                const res = await fetch('http://localhost:3000/api/user/create', {
-                    method: 'POST',
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(values)
-                })
-                if (!res.ok) setErrorMessage(res.statusText)
+            onSubmit={values => {
+                createUserMutation.mutate(values)
             }}>
             <Form className='flex flex-col w-full gap-4'>
                 <TextInput
@@ -31,7 +40,7 @@ export const RegisterForm = () => {
                     type='password'
                     required
                 />
-                {errorMessage !== '' && <p className='text-sm text-red_1 text-center'>{errorMessage}</p>}
+                {createUserMutation.isError && <p className='text-sm text-red_1 text-center'>{String(createUserMutation.error)}</p>}
                 <button className='bg-light_blue_1 font-medium transition p-2 w-full rounded-full leading-6 hover:bg-white_1 hover:text-red_1 mt-5' type='submit'>SIGN UP</button>
             </Form>
         </Formik>
