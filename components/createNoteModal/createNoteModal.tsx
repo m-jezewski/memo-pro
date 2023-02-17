1535909035ed4170feb8e20b993e98fb5f06d629
Karrius <Ecetris@gmail.com>
2023-02-17 01:44:45 +0100

    extracted from from createNoteModal and moved it to separate component for future reuse

--- components/createNoteModal/createNoteModal.tsx
+++ components/createNoteModal/createNoteModal.tsx
@@ -1,9 +1,8 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { Form, Formik } from "formik";
 import { useSession } from "next-auth/react";
 
 import { Modal } from "../modal/modal";
-import { TextInput } from "../textInput/textInput";
+import { NoteForm } from "../noteForm/noteForm";
 
 import type { Dispatch, SetStateAction } from "react";
 
@@ -42,46 +41,12 @@ export const CreateNoteModal = ({ isOpen, setIsOpen }: CreateNoteModalProps) => 
             title='New note'
             overlay={true}
         >
-            <Formik
+            <NoteForm
                 initialValues={{ title: '', content: '' }}
-                onSubmit={(values, actions) => {
-                    createNoteMutation.mutate(values)
-                    actions.resetForm()
-                }}
-                validate={(values) => {
-                    if (!values.content) {
-                        return { content: 'Note content is required' }
-                    }
-                    return {}
-                }}
-            >
-                <Form className='w-full flex flex-col gap-4'>
-                    <TextInput
-                        label={<>Title <span className='text-light_blue_1'>(optional)</span></>}
-                        name='title'
-                        type='text'
-                        maxLength={40}
-                        placeholder='Note title'
-                    />
-                    <TextInput
-                        label={'Note content'}
-                        name='content'
-                        as='textarea'
-                        required
-                        rows={7}
-                        maxLength={10000}
-                        placeholder="Your note..."
-                    />
-                    {createNoteMutation.isError && <p className='text-red_1 text-center'>Sorry!, Something went wrong!</p>}
-                    <button
-                        disabled={createNoteMutation.isLoading}
-                        className={`font-medium transition p-2 w-full rounded-full mt-4 leading-6 bg-light_blue_1 
-                        ${createNoteMutation.isLoading ? 'bg-dark_blue_1' : 'hover:bg-white_1 hover:text-red_1'}`}
-                        type='submit'>
-                        {createNoteMutation.isLoading ? 'Creating new note...' : 'Add new note'}
-                    </button>
-                </Form>
-            </Formik>
+                mutation={createNoteMutation}
+                btnMessageIdle="Add new note"
+                btnMessageSubmitting="Creating new note..."
+            />
         </Modal>
     );
 }
---
+++ components/noteForm/noteForm.tsx
@@ -0,0 +1,67 @@
+import { Form, Formik } from "formik";
+
+import { TextInput } from "../textInput/textInput";
+
+import type { UseMutationResult } from '@tanstack/react-query';
+
+interface formValues {
+    readonly title: string
+    readonly content: string
+}
+
+interface NoteFormProps {
+    readonly mutation: UseMutationResult<void, unknown, formValues, unknown>
+    readonly initialValues: {
+        readonly title: string
+        readonly content: string
+    }
+    readonly btnMessageSubmitting: string
+    readonly btnMessageIdle: string
+}
+
+export const NoteForm = ({ mutation, initialValues, btnMessageIdle, btnMessageSubmitting }: NoteFormProps) => {
+
+    return (
+        <Formik
+            initialValues={initialValues}
+            onSubmit={(values, actions) => {
+                mutation.mutate(values)
+                actions.resetForm()
+            }}
+            validate={(values) => {
+                if (!values.content) {
+                    return { content: 'Note content is required' }
+                }
+                return {}
+            }}
+        >
+            <Form className='w-full flex flex-col gap-4'>
+                <TextInput
+                    label={<>Title <span className='text-light_blue_1'>(optional)</span></>}
+                    name='title'
+                    type='text'
+                    maxLength={40}
+                    placeholder='Note title'
+                />
+                <TextInput
+                    label={'Note content'}
+                    name='content'
+                    as='textarea'
+                    required
+                    rows={7}
+                    maxLength={10000}
+                    placeholder="Your note..."
+                />
+                {mutation.isError && <p className='text-red_1 text-center'>Sorry!, Something went wrong!</p>}
+                <button
+                    disabled={mutation.isLoading}
+                    className={`font-medium transition p-2 w-full rounded-full mt-4 leading-6 bg-light_blue_1 
+                        ${mutation.isLoading ? 'bg-dark_blue_1' : 'hover:bg-white_1 hover:text-red_1'}`}
+                    type='submit'>
+                    {mutation.isLoading ? btnMessageSubmitting : btnMessageIdle}
+                </button>
+            </Form>
+        </Formik>
+    );
+}
+
