91bb3469e23a49d0e794e848267a8fec03741c03
Karrius <Ecetris@gmail.com>
2023-02-17 02:14:28 +0100

    edit (PUT) api route

---
+++ pages/api/note/edit.ts
@@ -0,0 +1,32 @@
+import type { Note } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { client } from "@/lib/prismadb";
+
+interface reqBody {
+    readonly id: string,
+    readonly title: string,
+    readonly content: string,
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Note>) {
+    const {id, title, content}: reqBody = req.body
+
+    if(req.method === 'PUT'){
+        try{
+            const note = await client.note.update({
+                where: { id: id },
+                data: {
+                    title: title,
+                    content: content
+                }
+            })
+            res.status(200).json(note)
+        } catch {
+            res.statusMessage = 'Sorry!, Something went wrong while updating your note.'
+            res.status(500).end()
+        }
+    } else{
+        res.status(405).end()
+    }
+}
