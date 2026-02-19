84a1773e353dce50ed5f4838dd13bc8ea1902c1d
Maciej <Ecetris@gmail.com>
2026-02-20 00:12:13 +0100

    Removed manual refetch from get notes query.

--- hooks/useGetNotes.ts
+++ hooks/useGetNotes.ts
@@ -1,25 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
 
 import type { Note } from '@prisma/client';
 
 export const useGetNotes = () => {
   const session = useSession();
+  const uid = session.data?.user.uid;
 
-  const noteQuery = useQuery({
-    queryKey: ['notes'],
+  return useQuery({
+    queryKey: ['notes', uid],
     queryFn: async () => {
-      if (session.data?.user.uid === undefined) return null;
-      const res = await fetch(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/note/${session.data?.user.uid}`);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/note/${uid}`);
       const data: readonly Note[] = await res.json();
       return [...data].sort((a, b) => a.orderIndex - b.orderIndex);
     },
+    enabled: !!uid,
   });
-
-  useEffect(() => {
-    noteQuery.refetch({ queryKey: ['notes'] }).catch((err) => console.log(err));
-  }, [session.status]);
-
-  return noteQuery;
 };
