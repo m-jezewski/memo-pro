1dfe635e57518329bf18e08964730e40ff43ed0c
Maciej <Ecetris@gmail.com>
2026-02-20 00:02:07 +0100

    Stabilized query client in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
